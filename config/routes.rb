869a57e0d9cc20c1ae31b868e82238fe2a330a35
DmitriKudriashov <kds.120731@gmail.com>
2017-10-25 17:45:30 +0300

    before migration first

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  resources :modelprices
+  resources :inventories
+  resources :ratecurries
+  resources :currencies
+  resources :modelsets
+  resources :descriptions
+  resources :budgetitems
+  resources :products
+  resources :units
+  resources :brands
+  resources :categories
+  resources :suppliers
   resources :posts
 	root to: 'startpage#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
