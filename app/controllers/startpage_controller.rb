61ca4a70a2ad600883c564c6583e3f87ad9395a3
DmitriKudriashov <kds.120731@gmail.com>
2017-11-13 11:23:04 +0200

    Add authenticate

--- app/controllers/startpage_controller.rb
+++ app/controllers/startpage_controller.rb
@@ -1,9 +1,19 @@
 class StartpageController < ActionController::Base
+
+  before_filter :authenticate
+
   def index
   	
   end
   def show
   	
   end
-
+  def authenticate
+    if !@user_name
+      authenticate_or_request_with_http_basic do |username, password|
+        username == "user" && password == "xxx"
+       # @user_name = "user"
+      end
+    end
+  end
 end
