8ced6c4fdbf38fca3ea504ee0cd4b706776bd7bd
DmitriKudriashov <kds.120731@gmail.com>
2017-12-19 10:50:22 +0200

    change replace_name_budgetsitem_id__budgetitem_id

--- db/migrate/20171102120725_replace_name_budgetsitem_id__budgetitem_id.rb
+++ db/migrate/20171102120725_replace_name_budgetsitem_id__budgetitem_id.rb
@@ -1,6 +1,6 @@
 class ReplaceNameBudgetsitemIdBudgetitemId < ActiveRecord::Migration[5.0]
   def change
-  		rename_column :inventories, :budgetitem_id, :budgetitem_id
+  		#rename_column :inventories, :budgetitem_id, :budgetitem_id
   
   end
 end
